596ac58a598db32be599011a6b36be358e3c5400
keerthanamsys <149489753+keerthanamsys@users.noreply.github.com>
2024-12-12 18:42:15 +0530

    Update change_summary.go
    Signed-off-by: keerthanamsys <149489753+keerthanamsys@users.noreply.github.com>

--- internal/command/views/json/change_summary.go
+++ internal/command/views/json/change_summary.go
@@ -1,48 +1,60 @@
-// Copyright (c) The OpenTofu Authors
-// SPDX-License-Identifier: MPL-2.0
-// Copyright (c) 2023 HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package json
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Operation string
 
 const (
-    OperationApplied   Operation = "apply"
-    OperationDestroyed Operation = "destroy"
-    OperationPlanned   Operation = "plan"
+	OperationApplied   Operation = "apply"
+	OperationDestroyed Operation = "destroy"
+	OperationPlanned   Operation = "plan"
 )
 
 type ChangeSummary struct {
-    Add       int       `json:"add"`
-    Change    int       `json:"change"`
-    Import    int       `json:"import"`
-    Remove    int       `json:"remove"`
-    Forget    int       `json:"forget"`
-    Operation Operation `json:"operation"`
+	Add       int       `json:"add"`
+	Change    int       `json:"change"`
+	Import    int       `json:"import"`
+	Remove    int       `json:"remove"`
+	Forget    int       `json:"forget"`
+	Operation Operation `json:"operation"`
 }
 
 // The summary strings for apply and plan are accidentally a public interface
 // used by Terraform Cloud and Terraform Enterprise, so the exact formats of
 // these strings are important.
 func (cs *ChangeSummary) String() string {
-    switch cs.Operation {
-    case OperationApplied:
-        if cs.Import > 0 {
-            return fmt.Sprintf("Apply complete! Resources: %d imported, %d added, %d changed, %d destroyed, %d forgotten.", cs.Import, cs.Add, cs.Change, cs.Remove, cs.Forget)
-        }
-        return fmt.Sprintf("Apply complete! Resources: %d added, %d changed, %d destroyed, %d forgotten.", cs.Add, cs.Change, cs.Remove, cs.Forget)
-    case OperationDestroyed:
-        return fmt.Sprintf("Destroy complete! Resources: %d destroyed, %d forgotten.", cs.Remove, cs.Forget)
-    case OperationPlanned:
-        if cs.Import > 0 {
-            return fmt.Sprintf("Plan: %d to import, %d to add, %d to change, %d to destroy, %d to forget.", cs.Import, cs.Add, cs.Change, cs.Remove, cs.Forget)
-        }
-        return fmt.Sprintf("Plan: %d to add, %d to change, %d to destroy, %d to forget.", cs.Add, cs.Change, cs.Remove, cs.Forget)
-    default:
-        return fmt.Sprintf("%s: %d add, %d change, %d destroy, %d forget", cs.Operation, cs.Add, cs.Change, cs.Remove, cs.Forget)
-    }
+	var builder strings.Builder
+	switch cs.Operation {
+	case OperationApplied:
+		builder.WriteString("Apply complete! Resources: ")
+		if cs.Import > 0 {
+			builder.WriteString(fmt.Sprintf("%d imported, ", cs.Import))
+		}
+		builder.WriteString(fmt.Sprintf("%d added, %d changed, %d destroyed", cs.Add, cs.Change, cs.Remove))
+		if cs.Forget > 0 {
+			builder.WriteString(fmt.Sprintf(", %d forgotten.", cs.Forget))
+		} else {
+			builder.WriteString(".")
+		}
+		return builder.String()
+	case OperationDestroyed:
+		return fmt.Sprintf("Destroy complete! Resources: %d destroyed.", cs.Remove)
+	case OperationPlanned:
+		builder.WriteString("Plan: ")
+		if cs.Import > 0 {
+			builder.WriteString(fmt.Sprintf("%d to import, ", cs.Import))
+		}
+		builder.WriteString(fmt.Sprintf("%d to add, %d to change, %d to destroy", cs.Add, cs.Change, cs.Remove))
+		if cs.Forget > 0 {
+			builder.WriteString(fmt.Sprintf(", %d to forget.", cs.Forget))
+		} else {
+			builder.WriteString(".")
+		}
+		return builder.String()
+	default:
+		return fmt.Sprintf("%s: %d add, %d change, %d destroy", cs.Operation, cs.Add, cs.Change, cs.Remove)
+	}
 }
-
